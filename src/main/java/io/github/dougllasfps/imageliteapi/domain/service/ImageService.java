0b0e0565d3d591aa88882001aabef69eddfafc18
Leonardo <leonardo.calheiros@gmail.com>
2024-01-16 22:04:46 -0300

    feat: Implementing ImageService

---
+++ src/main/java/io/github/dougllasfps/imageliteapi/domain/service/ImageService.java
@@ -0,0 +1,11 @@
+package io.github.dougllasfps.imageliteapi.domain.service;
+
+import io.github.dougllasfps.imageliteapi.domain.entity.Image;
+
+import java.util.Optional;
+
+public interface ImageService {
+    Image save(Image image);
+
+    Optional<Image> findById(String id);
+}
